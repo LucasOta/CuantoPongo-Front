7b819e2741b51f02e24a57d7e1fc93bfc0bf1593
LucasOta <lucasmatiasota@gmail.com>
2019-10-27 19:47:55 -0300

    f

--- server.js
+++ server.js
@@ -21,6 +21,4 @@ app.use(function(req, res, next) {
 
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(process.env.PORT || 5000);
